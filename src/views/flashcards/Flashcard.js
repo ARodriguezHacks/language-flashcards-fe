94de0786ea91a1f5725d5e3d7a79f27177aac36d
Angie Rodriguez <angrodri902@gmail.com>
2021-05-31 23:29:59 -0700

    Configure Tailwind css and update starter folder structure

---
+++ src/views/flashcards/Flashcard.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Flashcard() {
+  const [flashcards, setFlashcards] = useState([]);
+
+  const getFlashcards = async () => {
+    const { data } = await axios.get("/flashcards");
+    console.log(data);
+    // return data.json();
+    setFlashcards(data);
+    localStorage.setItem("flashcards", JSON.stringify(data));
+  };
+
+  const onClick = () => {
+    getFlashcards();
+  };
+
+  // useEffect(() => {
+  //   getFlashcards();
+  // }, []);
+
+  return (
+    <div className="container sm">
+      <h1>Flashcards Page</h1>
+      {flashcards.length > 0 ? (
+        <ul>
+          {flashcards.map((flashcard) => (
+            <li key={flashcard.id}>{flashcard.en}</li>
+          ))}
+        </ul>
+      ) : (
+        <h4>No Flashcards! :( </h4>
+      )}
+      <button type="submit" onClick={onClick}>
+        Get Flashcards
+      </button>
+    </div>
+  );
+}
+
+export default Flashcard;
---
+++ src/views/landing/Landing.js
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+function Landing() {
+  return (
+    <div>
+      <h1>Flashcards Landing Page</h1>
+    </div>
+  );
+}
+
+export default Landing;
