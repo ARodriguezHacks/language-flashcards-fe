32c7de868b0d5428ace25177a36acb0c71f50784
Angie Rodriguez <angrodri902@gmail.com>
2021-06-12 20:10:45 -0700

    Fix Card flip

--- src/views/flashcards/Flashcard.js
+++ src/views/flashcards/Flashcard.js
@@ -3,65 +3,54 @@ import { useState } from "react";
 function Flashcard({ flashcardEn, flashcardEs }) {
   const [cardFront, setCardFront] = useState(true);
   const [cardBack, setCardBack] = useState(false);
+  const [addFlip, setAddFlip] = useState("");
 
-  const flipCard = (e) => {
-    // console.log(e);
-    setCardFront(!cardFront);
-    setCardBack(!cardBack);
+  const flipTopCard = () => {
+    setAddFlip("card-flip");
+  };
+
+  const flipBottomCard = () => {
+    setAddFlip("");
   };
 
   return (
     <li>
-      <div className="bg-gray-50 h-80 flex flex-col justify-center space-y-2 rounded relative">
-        {cardFront && (
-          <div
-            className={
-              cardFront
-                ? "z-10 h-80 absolute w-full"
-                : "-z-10 h-80 absolute w-full"
-            }
-          >
-            <h4 className="text-center text-2xl leading-normal p-4">
-              {flashcardEs}
-            </h4>
-            <div className="flex justify-around">
-              <button className="bg-yellow-200 p-2 rounded hover:text-gray-50 transition duration-500 ease-in-out transform hover:bg-yellow-400 hover:scale-110">
-                Get a Hint
-              </button>
-              <button
-                className="p-2 rounded hover:text-gray-50 transition bg-red-400 duration-500 ease-in-out transform hover:bg-red-600 hover:scale-110"
-                onClick={flipCard}
-              >
-                Get Answer
-              </button>
-            </div>
+      <div
+        className={`${addFlip} card bg-gray-50 h-80 flex flex-col justify-center gap-1 rounded relative`}
+      >
+        <div className="h-80 absolute w-full top-card bg-blue-300">
+          <h4 className="text-center text-2xl leading-normal p-4">
+            {flashcardEs}
+          </h4>
+          <div className="flex justify-around">
+            <button className="bg-yellow-200 p-2 rounded hover:text-gray-50 transition duration-500 ease-in-out transform hover:bg-yellow-400 hover:scale-110">
+              Get a Hint
+            </button>
+            <button
+              className="click p-2 rounded hover:text-gray-50 transition bg-red-400 duration-500 ease-in-out transform hover:bg-red-600 hover:scale-110"
+              onClick={flipTopCard}
+            >
+              Get Answer
+            </button>
           </div>
-        )}
+        </div>
 
-        {cardBack && (
-          <div
-            className={
-              cardBack
-                ? "z-1 h-80 absolute w-full"
-                : "-z-1 h-80 absolute w-full"
-            }
-          >
-            <h4 className="text-center text-2xl leading-normal p-4">
-              {flashcardEn}
-            </h4>
-            <div className="flex justify-around">
-              <button className="bg-yellow-200 p-2 rounded hover:text-gray-50 transition duration-500 ease-in-out transform hover:bg-yellow-400 hover:scale-110">
-                Get a Hint
-              </button>
-              <button
-                className="p-2 rounded hover:text-gray-50 transition bg-red-400 duration-500 ease-in-out transform hover:bg-red-600 hover:scale-110"
-                onClick={flipCard}
-              >
-                Flip Back
-              </button>
-            </div>
+        <div className="h-80 absolute w-full bottom-card bg-pink-200">
+          <h4 className="text-center text-2xl leading-normal p-4">
+            {flashcardEn}
+          </h4>
+          <div className="flex justify-around">
+            <button className="bg-yellow-200 p-2 rounded hover:text-gray-50 transition duration-500 ease-in-out transform hover:bg-yellow-400 hover:scale-110">
+              Get a Hint
+            </button>
+            <button
+              className="p-2 rounded hover:text-gray-50 transition bg-red-400 duration-500 ease-in-out transform hover:bg-red-600 hover:scale-110"
+              onClick={flipBottomCard}
+            >
+              Flip Back
+            </button>
           </div>
-        )}
+        </div>
       </div>
     </li>
   );
